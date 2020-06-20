3e141b4dcb05af73936345aa372256580f66515e
Tin Mijatovic <tin.mijatovic@codecentric.de>
2020-06-20 18:56:43 +0200

    Make GET RECT logo appear only if no args are passed

--- src/index.js
+++ src/index.js
@@ -6,30 +6,30 @@ const figlet = require("figlet");
 const chalk = require("chalk");
 const functions = require("./functions");
 
-figlet("GET REKT", (err, data) => {
-  if (err) {
-    console.log("Something went wrong...");
-    console.dir(err);
-    return;
-  }
-  console.log(chalk.cyan.bold(data));
+program.option("-c, --component <name>", "Create React component");
+program.option("-t, --container <name>", "Create React container");
+program.option("-i, --init", "Initialize RKT");
 
-  program.option("-c, --component <name>", "Create React component");
-  program.option("-t, --container <name>", "Create React container");
-  program.option("-i, --init", "Initialize RKT");
+program.parse(process.argv);
+if (program.component) functions.generateComponent();
+else if (program.container) functions.generateContainer();
+else if (program.init) functions.init();
+else {
+  figlet("GET REKT", (err, data) => {
+    if (err) {
+      console.log("Something went wrong...");
+      console.dir(err);
+      return;
+    }
+    console.log(chalk.cyan.bold(data));
 
-  program.parse(process.argv);
-  if (program.component) functions.generateComponent();
-  else if (program.container) functions.generateContainer();
-  else if (program.init) functions.init();
-  else {
     inquirer
       .prompt([
         {
           type: "list",
           name: "create",
           message: "What would you like to create?",
-          choices: ["Component", "Container"],
+          choices: ["Component", "Container"]
         },
         {
           type: "list",
@@ -49,5 +49,5 @@ figlet("GET REKT", (err, data) => {
       .then(answers => {
         console.log(JSON.stringify(answers, null, "  "));
       });
-  }
-});
+  });
+}
