0c30482ca0e6130d6a22f703d7de52a53b64afb9
Tin Mijatovic <tin.mijatovic@codecentric.de>
2020-06-21 22:44:02 +0200

    Add functionality to create new directorys if option selected is --init

--- src/functions/initRct.js
+++ src/functions/initRct.js
@@ -2,14 +2,24 @@ const fs = require("fs");
 module.exports = () => {
   fs.access("src/containers", error => {
     if (error) {
-      console.log("Create a CONTAINERS");
+      if (!fs.existsSync("src")) {
+        fs.mkdirSync("src");
+      }
+      if (!fs.existsSync("src/containers")) {
+      fs.mkdirSync("src/containers");
+      }
     } else {
       console.log("File alredy exists");
     }
   });
   fs.access("src/components", error => {
     if (error) {
-      console.log("Create a COMPONENTS");
+      if (!fs.existsSync("src")) {
+        fs.mkdirSync("src");
+      }
+      if (!fs.existsSync("src/components")) {
+      fs.mkdirSync("src/components");
+      }
     } else {
       console.log("File alredy exists");
     }
