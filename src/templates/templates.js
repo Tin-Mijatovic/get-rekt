d8cbf5d5d882d20e9bef025bff84af47cb8e66d3
Tin Mijatovic <tin.mijatovic@codecentric.de>
2020-08-17 10:05:22 +0200

    new format of functional and calass components

--- src/templates/templates.js
+++ src/templates/templates.js
@@ -1,7 +1,8 @@
 module.exports = {
   functional:
-    "import React from 'react'\r\n\r\nexport default function {{name}}() {\r\n  return (\r\n    <div>\r\n      \r\n    </div>\r\n  )\r\n}\r\n",
+    'import React from "react";\r\n\r\nfunction {{name}}() {\r\n  return <div></div>;\r\n}\r\n\r\nexport default {{name}};\r\n',
   class:
-    "import React, { Component } from 'react';\r\n\r\nexport default class {{name}} extends Component {\r\n  render() {\r\n    return <div>\r\n      \r\n    </div>;\r\n  }\r\n}\r\n",
-  index: "import {{name}} from './{{name}}';\r\n\r\nexport default {{name}};\r\n"
+    'import React, { Component } from "react";\r\n\r\nclass snippet extends Component {\r\n  render() {\r\n    return <div></div>;\r\n  }\r\n}\r\nexport default snippet;',
+  index:
+    "import {{name}} from './{{name}}';\r\n\r\nexport default {{name}};\r\n",
 };
